e5b0c0f31ed11a8b1644767747eacf5fce0b5333
x-hunter12 <154734962+x-hunter12@users.noreply.github.com>
2023-12-30 21:51:07 +0700

    Update xbot.sh

--- xbot.sh
+++ xbot.sh
@@ -30,7 +30,7 @@ pip3 install -r kyt/requirements.txt
 clear
 echo ""
 echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
-echo -e "$u │ \e[1;97;101m                   ADD BOT PANEL              ${NC} ${u}│${NC}"
+echo -e "$u │ \e[1;97;101m                  ADD BOT PANEL               ${NC} ${u}│${NC}"
 echo -e "$u └────────────────────────────────────────────────┘${NC}"
 echo -e "$u ┌────────────────────────────────────────────────┐${NC}"
 echo -e "$u │ ${g}Tutorial Creat Bot and ID Telegram                   ${NC}"
