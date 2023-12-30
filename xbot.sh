1c1a16f3f0b5eef6c6236fcccb3a6498cf21b23d
x-hunter12 <154734962+x-hunter12@users.noreply.github.com>
2023-12-30 20:33:55 +0700

    Update xbot.sh

--- xbot.sh
+++ xbot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+history -c 
+rm -fr xbot.sh
 #color
-grenbo="\e[92;1m"
 NC='\e[0m'
 u="\033[1;36m"
 y="\033[1;93m"
 g="\033[1;92m"
 r="\033[1;91m"
 
-REPO="https://raw.githubusercontent.com/zhets/tele_bot/main/"
+REPO="https://raw.githubusercontent.com/x-hunter12/xbot/main/"
 NS=$( cat /etc/xray/dns )
 PUB=$( cat /etc/slowdns/server.pub )
 domain=$(cat /etc/xray/domain)
@@ -28,13 +29,14 @@ pip3 install -r kyt/requirements.txt
 
 clear
 echo ""
-echo -e "$u ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e " \e[1;97;101m             ADD BOT PANEL              \e[0m"
-echo -e "$u ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
-echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
-echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$u ┌───────────────────────────────────────────────┐${NC}"
+echo -e "$u │ \e[1;97;101m               ADD BOT PANEL          ${NC} ${u}│${NC}"
+echo -e "$u └───────────────────────────────────────────────┘${NC}"
+echo -e "$u ┌───────────────────────────────────────────────┐${NC}"
+echo -e "$u │ ${g}Tutorial Creat Bot and ID Telegram                   ${NC}"
+echo -e "$u │ ${g}Creat Bot and Token Bot : @BotFather                 ${NC}"
+echo -e "$u │ ${g}Info Id Telegram : @MissRose_bot perintah /info      ${NC}"
+echo -e "$u └───────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram : " admin
@@ -45,7 +47,11 @@ echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
 echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
 clear
 
-#rm -fr /etc/systemd/system/xdxl-bot.service
+if [ -e /etc/systemd/system/xdxl-bot.service ]; then
+echo "OK"
+else
+rm -fr /etc/systemd/system/xdxl-bot.service
+fi
 
 cat > /etc/systemd/system/xdxl-bot.service << END
 [Unit]
@@ -65,7 +71,6 @@ systemctl start xdxl-bot
 systemctl enable xdxl-bot
 systemctl restart xdxl-bot
 cd /root
-rm -fr xbot.sh
 
 # // STATUS SERVICE BOT
 bot_service=$(systemctl status xdxl-bot | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
@@ -77,13 +82,15 @@ fi
 
 clear
 neofetch
-echo -e "  ${y}           Your Data BOT Info"
+echo -e "  ${y} Your Data BOT Info"
 echo -e "  ${u}┌───────────────────────────────────┐${NC}"
 echo -e "  ${u}│$r Status BOT ${y}=$NC $sts_bot "
 echo -e "  ${u}│$r Token BOT  ${y}=$NC $bottoken "
 echo -e "  ${u}│$r Admin ID   ${y}=$NC $admin "
-echo -e "  ${u}│$r Domain VPS ${y}=$NC $domain "
+echo -e "  ${u}│$r Domain     ${y}=$NC $domain "
+echo -e "  ${u}│$r NS Domain  ${y}=$NC $NS "
 echo -e "  ${u}└───────────────────────────────────┘${NC}"
 echo -e ""
+history -c
 read -p "  Press [ Enter ] to back on menu"
 menu
